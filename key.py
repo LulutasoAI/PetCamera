f1387bb1f1842e0f8ab92eee1017d8cceab3db4f
Lulu-Gustav der Große <58521969+LulutasoAI@users.noreply.github.com>
2021-07-22 18:36:17 +0900

    Add files via upload

---
+++ key.py
@@ -0,0 +1,3 @@
+app = "yoursecretkeyfordiscordbothere"
+passlist = ["gimmepic"] #keyword to send. to activate the programme to send you the pic.
+allowedusers = ["username#0000"] #the list of users who can activate the programme
